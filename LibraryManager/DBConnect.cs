153cfc22815ce46edca142d15dfc54a5e18ae8a4
aswin vp <aswinprabhakaran@gmail.com>
2016-10-18 17:02:25 +0530

    Login OK

---
+++ LibraryManager/DBConnect.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace LibraryManager
+{
+    public static class DBConnect
+    {
+        //private static string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\node\DB\TTGDB.mdf;Integrated Security=True;Connect Timeout=30";
+        private static string ConnectionString = ConfigurationSettings.AppSettings["ConnectionString"];
+        public static string ErrorMessage = "";
+        private static bool status = false;
+        private static SqlConnection conn;
+        private static SqlDataAdapter Adap;
+        private static SqlCommand sqlcommand;
+        public static bool Connect()
+        {
+            try
+            {
+                conn = new SqlConnection(ConnectionString);
+                conn.Open();
+                ErrorMessage = "";
+                status = true;
+                return true;
+            }
+            catch (Exception exc)
+            {
+                ErrorMessage = "Cant connect database";
+                return false;
+            }
+        }
+
+        public static bool Disconnect()
+        {
+            try
+            {
+                conn.Close();
+                ErrorMessage = "";
+                status = false;
+                return true;
+            }
+            catch (Exception)
+            {
+                ErrorMessage = "Cant disconnect database";
+                return false;
+            }
+        }
+
+        public static bool isConnected()
+        {
+            return status;
+        }
+        public static int Insert(string command)
+        {
+            try
+            {
+                sqlcommand = new SqlCommand(command, conn);
+                int numRows = sqlcommand.ExecuteNonQuery();
+                ErrorMessage = "";
+                return numRows;
+            }
+            catch (Exception)
+            {
+                ErrorMessage = "error on insertion";
+                return 0;
+            }
+        }
+        public static int Update(string command)
+        {
+            try
+            {
+                sqlcommand = new SqlCommand(command, conn);
+                int numRows = sqlcommand.ExecuteNonQuery();
+                ErrorMessage = "";
+                return numRows;
+            }
+            catch (Exception)
+            {
+                ErrorMessage = "error on updation";
+                return 0;
+            }
+        }
+        public static int Delete(string command)
+        {
+            try
+            {
+                sqlcommand = new SqlCommand(command, conn);
+                int numRows = sqlcommand.ExecuteNonQuery();
+                ErrorMessage = "";
+                return numRows;
+            }
+            catch (Exception)
+            {
+                ErrorMessage = "error on delete";
+                return 0;
+            }
+        }
+        public static bool Drop(string command)
+        {
+            try
+            {
+                sqlcommand = new SqlCommand(command, conn);
+                sqlcommand.ExecuteNonQuery();
+                ErrorMessage = "";
+                return true;
+            }
+            catch (Exception)
+            {
+                ErrorMessage = "cant drop";
+                return false;
+            }
+        }
+        public static bool Create(string command)
+        {
+            try
+            {
+                sqlcommand = new SqlCommand(command, conn);
+                sqlcommand.ExecuteNonQuery();
+                ErrorMessage = "";
+                return true;
+            }
+            catch (Exception)
+            {
+                ErrorMessage = "cant create";
+                return false;
+            }
+        }
+        public static DataTable Select(string command)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                Adap = new SqlDataAdapter(command, conn);
+                Adap.Fill(dt);
+                ErrorMessage = "";
+            }
+            catch (Exception)
+            {
+                ErrorMessage = "error on insertion";
+            }
+            return dt;
+        }
+    }
+}
--- LibraryManager/Login.xaml.cs
+++ LibraryManager/Login.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,5 +24,106 @@ namespace LibraryManager
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void login_mouseleftdwn(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        private void loginwindow_loadd(object sender, RoutedEventArgs e)
+        {
+            if (DBConnect.Connect())
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("unable to connect Database", "ERROR!", MessageBoxButton.OK, MessageBoxImage.Stop);
+                this.Close();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            bool canForward = true;
+            string username = txtUserName.Text;
+            string paswd = pswdField.Password;
+
+            if (txtUserName.Text == "")
+            {
+                txtUserName.BorderBrush = Brushes.Red;
+                lblNotif.Content = "enter user name ";
+                canForward = false;
+            }
+            else
+            {
+                txtUserName.BorderBrush = Brushes.Black;
+            }
+            if (pswdField.Password == "")
+            {
+                pswdField.BorderBrush = Brushes.Red;
+                if (canForward) lblNotif.Content = "password not valid !";
+                canForward = false;
+            }
+            else
+            {
+                pswdField.BorderBrush = Brushes.Black;
+            }
+            if (canForward)
+            {
+                bool success = false;
+                bool foundUName = false;
+                string type = "";
+                lblNotif.Content = "";
+                string query = string.Format("SELECT * FROM [dbo].[Users] WHERE UserName='" + username + "';");
+                DataTable dt = DBConnect.Select(query);
+                try
+                {
+                    if (dt.Rows[0][0].ToString() == username)
+                    {
+                        foundUName = true;
+                        if (dt.Rows[0][1].ToString() == paswd)
+                        {
+                            success = true;
+                            type = dt.Rows[0][2].ToString();
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+                if (foundUName)
+                {
+                    txtUserName.BorderBrush = Brushes.Black;
+                    if (success)
+                    {
+                        pswdField.BorderBrush = Brushes.Black;
+                        lblNotif.Content = "";
+                        // navigate to home pages of (type)
+                        if (type == "A")
+                        {
+                            //new AdminHomeWindow(username, name, type).Show();
+                            new MainWindow().Show();
+                            this.Close();
+                        }
+                    }
+                    else
+                    {
+                        pswdField.BorderBrush = Brushes.Red;
+                        lblNotif.Content = "incorrect password ";
+                    }
+                }
+                else
+                {
+                    lblNotif.Content = "Invalid user name. ";
+                }
+            }
+        }
+
     }
 }
