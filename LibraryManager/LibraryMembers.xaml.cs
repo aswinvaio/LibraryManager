8b7e40f9e8e85f4c9d42797ac25c9e77be524973
aswin vp <aswinprabhakaran@gmail.com>
2016-10-21 02:42:28 +0530

    Library member window

---
+++ LibraryManager/LibraryMembers.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryManager
+{
+    /// <summary>
+    /// Interaction logic for LibraryMembers.xaml
+    /// </summary>
+    public partial class LibraryMembers : Window
+    {
+        DataTable dt;
+
+        public LibraryMembers()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DBConnect.isConnected() == false)
+                DBConnect.Connect(); //only when starting from this wndw
+            string query = string.Format("SELECT [MemberId],[Name],[Phone],[Email],[Address] FROM Members;");
+            dt = DBConnect.Select(query);
+            dt.Columns[0].ColumnName = "Member Id";
+            dt.Columns[1].ColumnName = "Name";
+            dt.Columns[2].ColumnName = "Phone Number";
+            dt.Columns[3].ColumnName = "Email";
+            dt.Columns[4].ColumnName = "Address";
+
+            dgMemberss.ItemsSource = dt.DefaultView;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnremove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnedit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnadd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
--- LibraryManager/MainWindow.xaml.cs
+++ LibraryManager/MainWindow.xaml.cs
@@ -24,5 +24,47 @@ namespace LibraryManager
         {
             InitializeComponent();
         }
+
+        private void picMembers_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            LibraryMembers lm = new LibraryMembers();
+            lm.ShowDialog();
+        }
+
+        private void picMembers_MouseEnter(object sender, MouseEventArgs e)
+        {
+            picMembers.Opacity = .5;
+        }
+
+        private void picMembers_MouseLeave(object sender, MouseEventArgs e)
+        {
+            picMembers.Opacity = 1;
+        }
+
+        private void AdminHome_loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lblLogout_mld(object sender, MouseButtonEventArgs e)
+        {
+            new Login().Show();
+            this.Close();
+        }
+
+        private void lblLogout_me(object sender, MouseEventArgs e)
+        {
+            lblLogout.Opacity = .5;
+        }
+
+        private void lblLogout_mlv(object sender, MouseEventArgs e)
+        {
+            lblLogout.Opacity = 1;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
